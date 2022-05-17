271bb73b7c6b122607eb3a4122211b4e03e4ff87
unknown <jules.latapy@gmail.com>
2022-05-17 23:21:40 +0200

    fix scripts

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -2,4 +2,5 @@
 (echo "activating venv with conda" && conda activate ./.venv) || \
     #if it fails : with .venv
     (echo "activating venv with venv" . ./.venv/Scripts/activate) || \
-    (echo "Error : no venv activated");
+        (echo "activating venv with venv" . ./.venv/bin/activate) || \
+            (echo "Error : no venv activated");
--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -2,5 +2,6 @@
 (echo "creating venv with conda" && conda create -y --prefix .venv pip python=3.7) || \
     #if it fails : with .venv
     (echo "creating venv with venv" && python -m venv .venv && . ./.venv/Scripts/activate && pip install --upgrade pip) || \
-    (m -rf .venv && echo "Error : no venv created");
+        (echo "creating venv with venv" && python3 -m venv .venv && . ./.venv/Scripts/activate && pip install --upgrade pip) || \
+            (m -rf .venv && echo "Error : no venv created");
 
