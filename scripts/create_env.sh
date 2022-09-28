8d8775aae6779b82e3495627d87d3b74ea3c27ce
unknown <jules.latapy@gmail.com>
2022-09-28 22:28:20 +0200

    create venv with yml

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,7 +1,8 @@
 #create venv with conda
-(echo "creating venv with conda" && conda create -y --prefix .venv pip python=3.7) || \
-    #if it fails : with .venv
-    (echo "creating venv with venv and python" && python -m venv .venv && . ./.venv/Scripts/activate && pip install --upgrade pip) || \
-        (echo "creating venv with venv and python3" && python3 -m venv .venv && . ./.venv/bin/activate && pip install --upgrade pip) || \
-            (rm -rf .venv && echo "Error : no venv created");
+(conda env create -y --prefix .venv pip python=3.7 -f environment.yml) || \
+    (echo "creating venv with conda" && conda create -y --prefix .venv pip python=3.7) || \
+        #if it fails : with .venv
+        (echo "creating venv with venv and python" && python -m venv .venv && . ./.venv/Scripts/activate && pip install --upgrade pip) || \
+            (echo "creating venv with venv and python3" && python3 -m venv .venv && . ./.venv/bin/activate && pip install --upgrade pip) || \
+                (rm -rf .venv && echo "Error : no venv created");
 
